b627ac267f9a3d3127d3b7a202d906127203bc50
Priyanka Gupta <priyug29@gmail.com>
2018-02-23 16:27:53 -0600

    Initial commit

---
+++ src/main/java/com/event/test/HelloWorld.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.event.test;
+
+/**
+ * @author priyanka
+ *
+ */
+public class HelloWorld {
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=5;i++) {
+			System.out.println("Hello World");
+
+		}
+
+	}
+
+}
